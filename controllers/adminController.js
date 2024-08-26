2a8d9ffa28790cc12008f2d09cf63d889a9f0b85
Abenezer Daniel <aabenezzer@gmail.com>
2024-08-26 09:55:03 +0300

    finish user auth

--- controllers/adminController.js
+++ controllers/adminController.js
@@ -1,26 +1,37 @@
-const {User, joiRegisterSchema}  = require('../models/User');
+const { User, joiRegisterSchema } = require("../models/User");
 const overview = async (req, res) => {
   res.render("dashboard");
-}
+};
 const dashboard = async (req, res) => {
   res.send("you are admin");
-}
+};
 
 const registerGet = async (req, res) => {
-  res.render('create-user', {errMessage: null, createdUser: null});
-}
-const registerPost = async(req, res) => {
-  const {error} = joiRegisterSchema.validate(req.body);
-  if(error) {
+  res.render("create-user", { errMessage: null, createdUser: null });
+};
+const registerPost = async (req, res) => {
+  const { error } = joiRegisterSchema.validate(req.body);
+  if (error) {
     const errMessage = error.details[0].message ?? null;
-    return res.status(400).render('create-user', {errMessage:errMessage, createdUser: null });
+    return res
+      .status(400)
+      .render("create-user", { errMessage: errMessage, createdUser: null });
   } else {
-    const user = new User({
-      ...req.body
+    const user = User.findOne({ username: req.body.username });
+    if (user) {
+      errMessage = "the username already exist!!!";
+      return res
+        .status(400)
+        .render("create-user", { errMessage: errMessage, createdUser: null });
+    }
+    user = new User({
+      ...req.body,
     });
     await user.save();
-    return res.status(400).render('create-user', {errMessage:null, createdUser: user });
+    return res
+      .status(400)
+      .render("create-user", { errMessage: null, createdUser: user });
   }
   console.log(req.body);
-}
-module.exports = {dashboard, overview, registerGet, registerPost};
+};
+module.exports = { dashboard, overview, registerGet, registerPost };
